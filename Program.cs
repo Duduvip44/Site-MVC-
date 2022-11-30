307a120a02a1097813a50ee24fa787d8e439c4d7
Eduardo Sousa <100807646+Duduvip44@users.noreply.github.com>
2022-11-30 11:47:09 -0300

    Projeto MVC

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Bem Vindo a Plataforma Online de Cursos");
+
+
+
+
+var professorService = new ProfessorService();
+var coordenadorService = new CoordenadorService();
+var materiaService = new MateriaService();
+var extensaoService = new ExtesnsaoService();
+
+Console.WriteLine("Cadastro Professor (2 professores)");
+professorService.IniciarCadastro();
+professorService.IniciarCadastro();
+
+Console.WriteLine("Cadastro Coordenador (2 coordenadores)");
+coordenadorService.IniciarCadastro();
+coordenadorService.IniciarCadastro();
+
+Console.WriteLine("Cadastro Materia (2 materias)");
+materiaService.CriarMateria(professorService.ObterTodos());
+materiaService.CriarMateria(professorService.ObterTodos());
+
+static void MateriaCriada(object source, EventArgs e)
+{
+    MateriaService materia = (MateriaService)source;
+    Console.WriteLine("A matéria foi criada com sucesso!.");
+    Console.ReadLine();
+}
+
+materiaService.CriouMateria += new MateriaService.MateriaCriadaEventHandler(MateriaCriada);
+materiaService.CriouMateria += professorService.OnCriouMateria;
+materiaService.CriouMateria += coordenadorService.OnCriouMateria;
+
+Console.WriteLine("Cadastro Extensao (2 extensoes)");
+extensaoService.CriarExtensao(professorService.ObterTodos());
+extensaoService.CriarExtensao(professorService.ObterTodos());
+
---
+++ src/Services/MateriaService.cs
@@ -0,0 +1,61 @@
+using PlataformaCurso.Models;
+
+public class MateriaService
+{
+    private List<Materia> ListaDeMaterias = new List<Materia>();
+
+    public delegate void MateriaCriadaEventHandler(object source, EventArgs e);
+
+    public void CriarMateria(List<Professor> professoresPossiveis)
+    {
+        Materia novaMateria;
+
+        Console.WriteLine("Digite o nome da matéria");
+        var nome = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(nome))
+        {
+            var professorResponsavel = SelecionaProfessorResponsavel(professoresPossiveis);
+
+            novaMateria = new Materia(nome, professorResponsavel);
+            Console.WriteLine("Materia cadastrada com sucesso!");
+            Console.ReadLine();
+        }
+
+        OnCriouMateria(new EventArgs());
+
+    }
+
+    public event MateriaCriadaEventHandler? CriouMateria;
+
+
+    public virtual void OnCriouMateria(EventArgs e)
+    {
+        if (CriouMateria != null)
+            CriouMateria(this,e);
+    }
+
+    private Professor? SelecionaProfessorResponsavel(List<Professor> professores)
+    {
+        Console.WriteLine("Escolha o professor responsável (SELECIONE A OPÇÂO)");
+
+        for(int i = 0; i < professores.Count; i++) {
+            Console.WriteLine($"{i+1} - {professores[i].Nome}");
+        }
+
+        var textoDigitado = Console.ReadLine();
+
+        if(!string.IsNullOrEmpty(textoDigitado))
+        {
+            var indice = int.Parse(textoDigitado) - 1;
+            return professores[indice];
+        }
+
+        return null;
+    }
+
+    public List<Materia> ObterTodos()
+    {
+        return this.ListaDeMaterias;
+    }
+}
